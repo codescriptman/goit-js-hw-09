b0e38d12aa1dc8ae3e2faa594059e324951fe480
Azazello <donazazello1312@gmail.com>
2025-07-27 22:09:49 +0300

    done

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -2,21 +2,15 @@ const formData = { email: '', message: '' };
 
 const form = document.querySelector('.feedback-form');
 const inp = document.querySelector('input');
-const txtar = document.querySelector('textarea');
+const textarea = document.querySelector('textarea');
 
 try {
-  const objStr = localStorage.getItem('feedback-form-state');
-  if (objStr === '') {
-    formData.email = '';
-    formData.message = '';
-  }
-  const obj = JSON.parse(objStr);
+  const obj = JSON.parse(localStorage.getItem('feedback-form-state'));
   formData.email = obj.email;
   formData.message = obj.message;
   console.log(formData);
   inp.value = obj.email;
-  txtar.value = obj.message;
-  localStorage.clear();
+  textarea.value = obj.message;
 } catch (error) {
   console.log(error.message);
 }
@@ -24,14 +18,15 @@ try {
 form.addEventListener('input', handle);
 
 function handle(event) {
-  formData.email = inp.value.trim();
-  formData.message = txtar.value.trim();
-  //   const email = form.elements.email.value;
-  //   const message = form.elements.message.value;
-  if (formData.email === '' || formData.message === '') {
-    console.log('Fill please all fields');
-  }
-  formData.email;
-  formData.message;
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  formData.email = inp.value.trim();
+  formData.message = textarea.value.trim();
 }
+
+form.addEventListener('submit', event => {
+  localStorage.clear();
+  form.reset();
+  if (formData.email === '' || formData.message === '') {
+    alert('Fill please all fields');
+  }
+});
